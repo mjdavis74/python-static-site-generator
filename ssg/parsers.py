9ef217484b8a1329f1e35ff384bd17613609085b
Matt Davis <mjdavis74@gmail.com>
2022-04-30 09:43:51 -0400

    Completed module

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -22,8 +22,7 @@ class Parser:
             file.write(content)
 
     def copy(self, path, source, dest):
-        shutil.copy2(path, dest / path relative_to(source))
-
+        shutil.copy2(path, dest / path.relative_to(source))
 
 class ResourceParser(Parser):
     extensions = [".jpg", ".png", ".gif", ".css", ".html"]
